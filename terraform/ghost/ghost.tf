2f6d7238741c51000c83dddd3731e28ce007faa6
Ronaldo Moura <ronaldo.moura@gmail.com>
2019-08-21 18:53:00 +0000

    Terraform file for ghost

---
+++ terraform/ghost/ghost.tf
@@ -0,0 +1,18 @@
+# Download the latest Ghost image
+resource "docker_image" "image_id" {
+  name = "ghost:latest"
+}
+
+# Start the Container
+resource "docker_container" "container_id" {
+  name  = "ghost_blog"
+  image = "${docker_image.image_id.latest}"
+  ports {
+    internal = "2368"
+    external = "80"
+  }
+}
+
+output "container_ip_addr" {
+  value = "${docker_container.container_id.ip_address}"
+}
